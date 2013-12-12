46d76cfad6b038448bf5f120925da4c69406b10c
B Krishna Chaitanya <bkchaitan94@gmail.com>
2013-12-12 21:10:29 +0530

    Team fluxus page added

--- js/index.js
+++ js/index.js
@@ -1,8 +1,31 @@
 // (function(a,b){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4)))window.location=b})(navigator.userAgent||navigator.vendor||window.opera,'http://flux.src/mobile');
 // if(window.innerWidth<800) window.open('http://flux.src/mobile','_self');
 
+var preloadimgs = [
+	'img/gateclosed.png',
+	'img/gateopen.png',
+	'img/sidebarbg.png',
+	'img/sidebarbg2.png'
+]
+var imgs = [];
+for (var i = 0; i < preloadimgs.length; i++) {
+	imgs.push(new Image());
+	imgs[i].src=preloadimgs[i];
+};
+
+var gateevent = function(event) {
+	if(event.pageY>72*window.innerHeight/100){
+		$("#gate")[0].src = "img/gateopen.png";
+		animateOnce('#loading','tada');
+	}
+	else $("#gate")[0].src = "img/gateclosed.png";
+};
+$(window).on("mousemove",gateevent);
+
 $('#content > div').hide();
 $('#modals > div').hide();
+$('.teamtop .details > div').hide();
+$('.teamtop .details div[data-id="0"]').show();
 window.onload = function(){
 	$('#home').show();
 	$('#loading').fadeOut(function(){
@@ -25,155 +48,6 @@ var animateOnce = function(a,b,c){
 		if(c!=undefined) c();
 	});
 };
-var preloadimgs = [
-	'img/gateclosed.png',
-	'img/gateopen.png',
-	'img/sidebarbg.png',
-	'img/sidebarbg2.png'
-]
-var imgs = [];
-for (var i = 0; i < preloadimgs.length; i++) {
-	imgs.push(new Image());
-	imgs[i].src=preloadimgs[i];
-};
-var chnginCnt = false;
-function openCntPg(a){
-	if(!chnginCnt&&events.current!=a){
-		chnginCnt = true;
-		$("#evt-img")[0].src=null;
-		$("#evt-abst").html('');
-		$("#evt-title").html('');
-		switch(a){
-			case "register":
-				$("#register").show();
-				animateOnce("#register",'bounceIn');
-				break;
-			case "tech":
-			case "cult":
-			case "workshop":
-			case "proshows":
-			case "team":
-				events.open(a);
-				break;
-			default:
-				events.current = 0;
-				$("#content > div").fadeOut(500);
-				setTimeout(function(){$("#"+a).fadeIn(500);},510);
-		}
-		chnginCnt = false;
-	}
-}
-var events = {
-	current: '',
-	currentJSON: '',
-	open: function (a){
-		if(events.current){
-			if(events.current!=a){
-				if($("#evt-sidebar")[0].style.cssFloat=='left'){
-					animateOnce("#evt-sidebar",'slideOutLeft',function(){
-						events.load(a);
-						$("#evt-sidebar")[0].style.cssFloat='right';
-						$("#evt-details")[0].style.cssFloat='left';
-						$("#evt-sidebar ul")[0].style.textAlign='right';
-						$("#evt-sidebar")[0].style.background = 'url("img/sidebarbg2.png")';
-						animateOnce("#evt-sidebar",'slideInRight');
-					});
-				} else {
-					animateOnce("#evt-sidebar",'slideOutRight',function(){
-						events.load(a);
-						$("#evt-sidebar")[0].style.cssFloat='left';
-						$("#evt-details")[0].style.cssFloat='right';
-						$("#evt-sidebar")[0].style.background = 'url("img/sidebarbg.png")';
-						animateOnce("#evt-sidebar",'slideInLeft');
-					});
-				}
-			}
-		}else{
-			events.current = -1;
-			$("#content > div").fadeOut(500);
-			$("#events").show(0);
-			events.load(a);
-			if($("#evt-sidebar")[0].style.cssFloat=='left'){
-				$("#evt-sidebar")[0].style.cssFloat='right';
-				$("#evt-details")[0].style.cssFloat='left';
-				$("#evt-sidebar ul")[0].style.textAlign='right';
-				$("#evt-sidebar")[0].style.background = 'url("img/sidebarbg2.png")';
-				animateOnce("#evt-sidebar",'slideInRight');
-			} else {
-				$("#evt-sidebar")[0].style.cssFloat='left';
-				$("#evt-details")[0].style.cssFloat='right';
-				$("#evt-sidebar")[0].style.background = 'url("img/sidebarbg.png")';
-				animateOnce("#evt-sidebar",'slideInLeft');
-			}
-		}
-		events.current=a;
-	},
-	load: function(a){
-		var jsonfile = '';
-		switch(a){
-			case "team":
-				$("#events")[0].style.background="rgba(0,0,0,0.85)";
-				jsonfile = 'data/team.json';
-				break;
-			case "proshows":
-				$("#events")[0].style.background="rgba(5,10,0,0.85)";
-				jsonfile = 'data/proshows.json';
-				break;
-			case "tech":
-				$("#events")[0].style.background="rgba(5,10,10,0.85)";
-				jsonfile = 'data/tech.json';
-				break;
-			case "workshop":
-				$("#events")[0].style.background="rgba(5,0,10,0.85)";
-				jsonfile = 'data/workshop.json';
-				break;
-			case "cult":
-				$("#events")[0].style.background="rgba(15,0,0,0.85)";
-				// jsonfile = 'https://googledrive.com/host/0B7gpUuZnCjpdVHRwWVgzZGt2bWM/events.json';
-				jsonfile = 'data/cult.json';
-				break;
-		}
-		$("#evt-sidebar").html("<ul></ul>");
-		// $("#evt-sidebar ul").append('<li class="evt-sel">asdasdasd</li>');
-		$.ajax({
-			dataType: "json",
-			url: jsonfile,
-			data: {},
-			success: function(data){
-				events.currentJSON=data;
-				for(var i=0;i<data.length;i++){
-					$("#evt-sidebar ul").append('<li data-id="'+i+'"">'+data[i].title+'</li>');
-				};
-				$("#evt-sidebar ul li").click(function(){
-					$("#evt-sidebar ul li").removeClass('evt-sel');
-					var t = $(this);
-					t.addClass('evt-sel');
-					var w = events.currentJSON[t[0].dataset.id];
-					// $(".evt-sel").animate({marginTop: t.style.top});
-					animateOnce("#evt-details",'slideOutUp',function(){
-						$("#evt-title").html(w.title);
-						$("#evt-img")[0].src = "http://lorempixel.com/700/300/technics";
-						$("#evt-abst").html(w.abstract);
-						if(w.rules)
-							$("#evt-abst").append('<br><br><div style="font: 120% Carnevalee;">RULES</div>'+w.rules);
-						$("#evt-abst").append(w.etc);
-						animateOnce("#evt-imgdiv",'slideInLeft');
-						animateOnce("#evt-abst",'slideInRight');
-						if(w.hasOwnProperty("contact")) console.log(1);
-					});
-				});
-			}
-		});
-	}
-};
-var gateevent = function(event) {
-	if(event.pageY>72*window.innerHeight/100){
-		$("#gate")[0].src = "img/gateopen.png";
-		animateOnce('#loading','tada');
-	}
-	else $("#gate")[0].src = "img/gateclosed.png";
-};
-$(window).on("mousemove",gateevent);
 
 var topic = function(titletext,name,img,hoverimg){
 	this.titletext = titletext;
@@ -247,6 +121,137 @@ var updateTopicPos = function(){
 	};
 }
 updateTopicPos();
+
+var chnginCnt = false;
+function openCntPg(a){
+	if(!chnginCnt&&events.current!=a){
+		chnginCnt = true;
+		$("#events .img")[0].src=null;
+		$("#events .abst").html('');
+		$("#events .title").html('');
+		switch(a){
+			case "register":
+				$("#register").show();
+				animateOnce("#register",'bounceIn');
+				break;
+			case "tech":
+			case "cult":
+			case "workshop":
+			case "proshows":
+				events.open(a);
+				break;
+			default:
+				events.current = 0;
+				$("#content > div").fadeOut(500);
+				setTimeout(function(){$("#"+a).fadeIn(500);},510);
+		}
+		chnginCnt = false;
+	}
+}
+var events = {
+	current: '',
+	currentJSON: '',
+	open: function (a){
+		if(events.current){
+			if(events.current!=a){
+				if($("#events .sidebar")[0].style.cssFloat=='left'){
+					animateOnce("#events .sidebar",'slideOutLeft',function(){
+						events.load(a);
+						$("#events .sidebar")[0].style.cssFloat='right';
+						$("#events .details")[0].style.cssFloat='left';
+						$("#events .sidebar ul")[0].style.textAlign='right';
+						$("#events .sidebar")[0].style.backgroundImage = 'url(img/sidebarbg2.png)';
+						animateOnce("#events .sidebar",'slideInRight');
+					});
+				} else {
+					animateOnce("#events .sidebar",'slideOutRight',function(){
+						events.load(a);
+						$("#events .sidebar")[0].style.cssFloat='left';
+						$("#events .details")[0].style.cssFloat='right';
+						$("#events .sidebar")[0].style.backgroundImage = 'url(img/sidebarbg.png)';
+						animateOnce("#events .sidebar",'slideInLeft');
+					});
+				}
+			}
+		}else{
+			events.current = -1;
+			$("#content > div").fadeOut(500);
+			$("#events").show(0);
+			events.load(a);
+			if($("#events .sidebar")[0].style.cssFloat=='left'){
+				$("#events .sidebar")[0].style.cssFloat='right';
+				$("#events .details")[0].style.cssFloat='left';
+				$("#events .sidebar ul")[0].style.textAlign='right';
+				$("#events .sidebar")[0].style.backgroundImage = 'url(img/sidebarbg2.png)';
+				animateOnce("#events .sidebar",'slideInRight');
+			} else {
+				$("#events .sidebar")[0].style.cssFloat='left';
+				$("#events .details")[0].style.cssFloat='right';
+				$("#events .sidebar")[0].style.backgroundImage = 'url(img/sidebarbg.png)';
+				animateOnce("#events .sidebar",'slideInLeft');
+			}
+		}
+		events.current=a;
+	},
+	load: function(a){
+		var jsonfile = '';
+		switch(a){
+			case "proshows":
+				$("#events")[0].style.background="rgba(5,10,0,0.85)";
+				jsonfile = 'data/proshows.json';
+				break;
+			case "tech":
+				$("#events")[0].style.background="rgba(5,10,10,0.85)";
+				jsonfile = 'data/tech.json';
+				break;
+			case "workshop":
+				$("#events")[0].style.background="rgba(5,0,10,0.85)";
+				jsonfile = 'data/workshop.json';
+				break;
+			case "cult":
+				$("#events")[0].style.background="rgba(15,0,0,0.85)";
+				// jsonfile = 'https://googledrive.com/host/0B7gpUuZnCjpdVHRwWVgzZGt2bWM/events.json';
+				jsonfile = 'data/cult.json';
+				break;
+		}
+		$("#events .sidebar").html("<ul></ul>");
+		// $("#events .sidebar ul").append('<li class=".evt-sel">asdasdasd</li>');
+		$.ajax({
+			dataType: "json",
+			url: jsonfile,
+			data: {},
+			success: function(data){
+				events.currentJSON=data;
+				for(var i=0;i<data.length;i++){
+					$("#events .sidebar ul").append('<li data-id="'+i+'"">'+data[i].title+'</li>');
+				};
+				$("#events .sidebar ul li").click(function(){
+					$("#events .sidebar ul li").removeClass('.evt-sel');
+					var t = $(this);
+					t.addClass('.evt-sel');
+					var w = events.currentJSON[t[0].dataset.id];
+					// $(".evt-sel").animate({marginTop: t.style.top});
+					animateOnce("#events .details",'slideOutUp',function(){
+						$("#events .title").html(w.title);
+						$("#events .img")[0].src = "http://lorempixel.com/700/300/technics";
+						$("#events .abst").html(w.abstract);
+						if(w.rules)
+							$("#events .abst").append('<br><br><div style="font: 120% Carnevalee;">RULES</div>'+w.rules);
+						$("#events .abst").append(w.etc);
+						animateOnce("#events .imgdiv",'slideInLeft');
+						animateOnce("#events .abst",'slideInRight');
+					});
+				});
+			}
+		});
+	}
+};
+
+$('.teamtop li[data-id]').click(function(){
+	var a=$(this)[0].dataset.id;
+	$('.teamtop .details div[data-id]').hide();
+	$('.teamtop .details div[data-id='+a+']').show();
+});
 
 $(window).resize(function(){
 	if(window.innerWidth<800) window.open('http://flux.src/mobile','_self');
